40a30e867035d46fef43c68b96d1ef1d065589e0
mangatur <mangatur.aritonang@gmail.com>
2018-10-15 20:32:31 +0700

    Direct db to mlab and add favicon.ico

--- base/config.js
+++ base/config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     port: 8100,
-    dbconn: 'mongodb://localhost:27017/workshopdb',
-    dbname: 'workshopdb',
-    enableAuth: false,
+    dbconn: 'mongodb://admin:admin12345@ds233323.mlab.com:33323/posdb',
+    dbname: 'posdb',
+    enableAuth: true,
     jwt_secret: 'secret1234',
     userCount: 2
 }
