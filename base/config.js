062bba61cbf1bae8deb6b2c9e5ffd80504794b45
mangatur <mangatur.aritonang@gmail.com>
2018-10-21 23:32:15 +0700

    Modify config

--- base/config.js
+++ base/config.js
@@ -3,7 +3,7 @@
 module.exports = {
     port: process.env.PORT || 8500,
     dbconn: process.env.MLAB || 'mongodb://localhost:27017/workshopdb',
-    dbname: process.env.DBNAME || 'workshopdb',
+    dbname: process.env.DB_NAME || 'workshopdb',
     enableAuth: true,
     expiresIn: 86400,
     jwt_secret: process.env.JWT_SECRET || 'secret1234',
