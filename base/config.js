337298f0b396eed69907e71752547a1e9373cbce
Atur <mangatur.aritonang@gmail.com>
2019-07-24 19:07:38 +0700

    mengembalikan enableAuth

--- base/config.js
+++ base/config.js
@@ -4,7 +4,7 @@ module.exports = {
     port: process.env.PORT || 8500,
     dbconn: process.env.MLAB || 'mongodb://localhost:27017/workshopdb',
     dbname: process.env.DB_NAME || 'workshopdb',
-    enableAuth: false,
+    enableAuth: JSON.parse(process.env.ENABLE_AUTH) || false,
     expiresIn: 86400,
     jwt_secret: process.env.JWT_SECRET || 'secret1234',
     userCount: 2
