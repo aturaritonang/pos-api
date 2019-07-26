6a8efd926e01e749ab0cc97f42433b24ecfc41bb
Atur <mangatur.aritonang@gmail.com>
2019-07-26 12:13:40 +0700

    Balikin ke settingan sebelumnya

--- base/config.js
+++ base/config.js
@@ -4,20 +4,10 @@ module.exports = {
     port: process.env.PORT || 8500,
     dbconn: process.env.MLAB || 'mongodb://localhost:27017/workshopdb',
     dbname: process.env.DB_NAME || 'workshopdb',
-    enableAuth: getEnableAuth() || false,
+    enableAuth: JSON.parse(process.env.ENABLE_AUTH) || false,
     expiresIn: 86400,
     jwt_secret: process.env.JWT_SECRET || 'secret1234',
     userCount: 2
 }
 
-function getEnableAuth() {
-    if (process.env.ENABLE_AUTH === null) {
-        return true
-    } else if (process.env.ENABLE_AUTH === undefined) {
-        return true
-    } else {
-        console.log('lain');
-        return process.env.ENABLE_AUTH
-    }
-}
 // JSON.parse(process.env.ENABLE_AUTH) ||
